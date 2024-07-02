f6f41a1e23698e6f8adeaa40a6a4e72f541b3635
anpaul7 <anpaul7@hotmail.com>
2024-07-02 00:08:14 -0500

    First deployment example project to deploy ERC20 contract with Hardhat at ArbitrumSepolia

---
+++ test/ERC20_Hardhat.ts
@@ -0,0 +1,35 @@
+
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("ERC20_HardHat", function () {
+
+
+  it("Test contract", async function () {
+    const ContractFactory = await ethers.getContractFactory("ERC20_HardHat");
+
+    const initialOwner = (await ethers.getSigners())[0].address;
+
+    const instance = await ContractFactory.deploy(initialOwner);
+    await instance.waitForDeployment();
+
+    expect(await instance.name()).to.equal("ERC20_HardHat");
+  });
+
+
+  it("Deploy transaction token contract", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+    const ERC20_HardHat = await ethers.getContractFactory("ERC20_HardHat");
+    const prueba = await ERC20_HardHat.deploy(owner.address);
+
+    // Transaction confirmation
+    await prueba.deploymentTransaction();
+
+    const mintAmount = ethers.parseUnits("1000", 18); 
+    await prueba.mint(addr1.address, mintAmount);
+    const balance = await prueba.balanceOf(addr1.address);
+    expect(balance).to.equal(mintAmount);
+    
+  });
+
+});
